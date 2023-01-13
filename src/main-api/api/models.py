a3411d5f949e09b4468c8153a0b85474a5b4e114
gamingflexer <osurve20ece@student.mes.ac.in>
2023-01-14 01:55:47 +0530

    models updated

--- src/main-api/api/models.py
+++ src/main-api/api/models.py
@@ -9,6 +9,7 @@ class User_info(models.Model):
 
 class Summary(models.Model):
     meeting_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User_info, on_delete=models.CASCADE)
     scheduled_meeting = models.CharField(max_length=255)
     scheduled_meeting_time = models.TimeField()
     meeting_description = models.CharField(max_length=255)
@@ -16,19 +17,28 @@ class Summary(models.Model):
     
     meeting_transcript = models.CharField(max_length=10000)
     meeting_audio_file_link = models.CharField(max_length=255)
+    is_multilingual = models.BooleanField(default=False)
+    language = models.CharField(max_length=255)
     
     is_summarized = models.BooleanField(default=False)
     meeting_summary = models.CharField(max_length=200)
     summary_gen_date = models.DateField()
+    reading_time = models.CharField(max_length=30) # in minutes & no of words
     meeting_category_assgined = models.CharField(max_length=255)
     model_used = models.CharField(max_length=255)
+    
     
     topic = models.CharField(max_length=255)
     top_keywords = models.CharField(max_length=255)
     top_speaker = models.CharField(max_length=255)
+    roles_detected = models.CharField(max_length=255)
     top_spent_time_person = models.CharField(max_length=255)
-    descions = models.CharField(max_length=1000)
+    descions = models.CharField(max_length=1000) #Action Items
     highlights = models.CharField(max_length=1000)
+    
+    #Feedbacks
+    is_good = models.BooleanField(default=False)
+    factual_consistency = models.CharField(max_length=255)
     
     meeting_summary_old = models.CharField(max_length=200)
     
