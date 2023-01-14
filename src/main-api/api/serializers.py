60ec2723dec6f3a4a7930fb2727be9eb8232e938
gamingflexer <osurve20ece@student.mes.ac.in>
2023-01-14 12:36:09 +0530

    code refactor

--- src/main-api/api/serializers.py
+++ src/main-api/api/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework import serializers
 from api.models import *
 
-class SummarySerializers(serializers.ModelSerializer):
+class Summary_Serializers(serializers.ModelSerializer):
     class Meta:
         model = Summary
         fields = "__all__"
         
-class User_infoSerializers(serializers.ModelSerializer):
+class User_info_Serializers(serializers.ModelSerializer):
     class Meta:
         model = User_info
         fields = "__all__"
