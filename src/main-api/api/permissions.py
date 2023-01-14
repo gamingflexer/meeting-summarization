c10602ea2ead141bb77066409540184dd299b682
gamingflexer <osurve20ece@student.mes.ac.in>
2023-01-14 12:35:53 +0530

    code refactor

--- src/main-api/api/permissions.py
+++ src/main-api/api/permissions.py
@@ -1,13 +1,13 @@
 from rest_framework import permissions
+from decouple import config
 
+DEBUG = config('DEBUG', cast=bool)
+
+# Custom permission to only allow owners of an object to edit it.
 class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
-
-from decouple import config
-
-DEBUG = config('DEBUG', cast=bool)
 
 def user_auth_required():
     if DEBUG:
