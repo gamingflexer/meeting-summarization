844fa4b103656e861e9121fc830136b41058db59
gamingflexer <osurve20ece@student.mes.ac.in>
2023-01-16 23:16:45 +0530

    added debug mode for mongodb

--- src/main-api/dp/settings.py
+++ src/main-api/dp/settings.py
@@ -87,24 +87,25 @@ WSGI_APPLICATION = 'dp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-"""DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}"""
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'dp',
-        'ENFORCE_SCHEMA': False,
-        'CLIENT': {
-                'host': MONGO_DB_URL
-
+if DEBUG == True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'dp',
+            'ENFORCE_SCHEMA': False,
+            'CLIENT': {
+                    'host': MONGO_DB_URL
+
+            }
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
