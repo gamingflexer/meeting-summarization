dd16c03783c7735579de882ba1463f2ad0a3841d
gamingflexer <osurve20ece@student.mes.ac.in>
2023-02-06 00:57:39 +0530

    struc changes

--- src/dp/dp/urls.py
+++ src/dp/dp/urls.py
@@ -21,7 +21,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="INCOME EXPENSES API",
+        title="DP API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.ourapp.com/policies/terms/",
