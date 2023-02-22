eaa4814abb131a1d178941685d1bd808b8f9ecb0
gamingflexer <osurve20ece@student.mes.ac.in>
2023-02-22 13:09:01 +0530

    model configs added

--- src/models-api/model/bart.py
+++
@@ -1,6 +0,0 @@
-from transformers import pipeline
-
-def bart_summarize(text,hub_model_id):
-    summarizer = pipeline("summarization", model="mikeadimech/longformer-qmsum-meeting-summarization")
-    summary = summarizer(text)
-    return summary[0]['summary_text']
