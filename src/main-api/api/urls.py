ba096ece7eed54ba78a1ae2a63a47b4bd036cffc
gamingflexer <osurve20ece@student.mes.ac.in>
2023-03-09 12:19:32 +0530

    code refactor & model added

--- src/main-api/api/urls.py
+++ src/main-api/api/urls.py
@@ -5,19 +5,28 @@ from .calender import GoogleCalendarInitView,GoogleCalendarEventsView,GoogleCale
 
 
 urlpatterns = [
+    # Landing Page API
     path('landing', LandingPageAPI.as_view(), name='landing'),
+    
+    # Meetings API
     path('addmeeting', AddMeetingAPI.as_view(), name='addmeeting'),
     path('addmeetingfile/<int:meeting_id>', AddMeetingFileAPI.as_view(), name='addmeetingfile'),
+    
+    # Summary API
     path('summary/<int:meeting_id>', SummaryPageAPI.as_view(), name='summary'),
     path('userinfo/<str:username>', EditUserDataAPI.as_view(), name='fetch_user_data'),
+    path('summary/<int:meeting_id>', SummaryPageAPI.as_view(), name='summary'),
+    path('summary/<int:meeting_id>/pdf', SummaryPageAPI.as_view(), name='summary_pdf'),
+    
+    # Google Calendar API
     path('rest/v1/calendar/init/',
          GoogleCalendarInitView.as_view(), name='calendar_init'),
     path('rest/v1/calendar/redirect/',
          GoogleCalendarRedirectView.as_view(), name='calendar_redirect'),
     path('rest/v1/calendar/events/',
          GoogleCalendarEventsView.as_view(), name='calendar_redirect'),
-    path('summary/<int:meeting_id>', SummaryPageAPI.as_view(), name='summary'),
-    path('summary/<int:meeting_id>/pdf', SummaryPageAPI.as_view(), name='summary_pdf'),
+    
+    # User API
     path('userinfo/<str:username>', EditUserDataAPI.as_view(), name='fetch_user_data')
 
 ] 
