6cf2df353b069f128e003c73898a75d3f4d3d986
gamingflexer <osurve20ece@student.mes.ac.in>
2023-03-10 14:47:24 +0530

    django models updated

--- src/main-api/api/models.py
+++ src/main-api/api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 
 class User_info(models.Model):
@@ -8,10 +8,12 @@ class User_info(models.Model):
     user_prof_type = models.CharField(max_length=255,default='SDE')
     user_meeting_category = models.CharField(max_length=255,default='tech')
 
-class Summary(models.Model):
+class Summary(models.Model): #all meeting data
     meeting_id = models.AutoField(primary_key=True)
     user_id = models.ForeignKey(User_info, on_delete=models.CASCADE)
+    title = models.CharField(default="",max_length=255)
     scheduled_meeting = models.CharField(max_length=255, null=True)
+    scheduled_meeting_date = models.DateField(default=datetime.date.today,blank=False)
     scheduled_meeting_time = models.TimeField(blank=True)
     meeting_description = models.CharField(max_length=255)
     meeting_location = models.CharField(max_length=255)
@@ -22,27 +24,43 @@ class Summary(models.Model):
     language = models.CharField(max_length=255)
     
     is_summarized = models.BooleanField(default=False,null=False)
-    meeting_summary = models.CharField(max_length=200)
+    meeting_summary = models.CharField(default= "",max_length=3000) #**Summary**
     summary_gen_date = models.DateField(null=True)
     reading_time = models.CharField(max_length=30) # in minutes & no of words
-    meeting_category_assgined = models.CharField(max_length=255)
+    meeting_category_assgined = models.CharField(default="general",max_length=255)
     model_used = models.CharField(max_length=255)
     
     
+    generated_title = models.CharField(default="",max_length=255)
     topic = models.CharField(max_length=255)
     top_keywords = models.CharField(max_length=255)
     top_speaker = models.CharField(max_length=255)
     roles_detected = models.CharField(max_length=255)
     top_spent_time_person = models.CharField(max_length=255)
     descions = models.CharField(max_length=1000) #Action Items
-    highlights = models.CharField(max_length=1000)
+    highlights = models.CharField(max_length=5000)
     
     #Feedbacks
     is_good = models.BooleanField(default=False)
     factual_consistency = models.CharField(max_length=255)
-    
     meeting_summary_old = models.CharField(max_length=200)
         
+class highlight_template(models.Model):
+    user_id = models.ForeignKey(User_info, on_delete=models.CASCADE)
+    hightlight_name = models.CharField(max_length=255)
+    highlight_desc = models.CharField(max_length=255)
+    keywords = models.CharField(max_length=255)
+    include_in_summary = models.BooleanField(default=False)
+    
+class analytics(models.Model):
+    overall_score = models.CharField(max_length=255) # how much meetings summary is good (0-100) in a pie chart        
+    # No of meetings per each day js
+    particapants_per = models.CharField(max_length=255) # no of participants in a meetings
+    punctuality_per = models.CharField(max_length=255) # how much meetings are joined on time
+    overtime_per = models.CharField(max_length=255) # how much meetings are going overtime
+    sentiment_overall = models.IntegerField() # how much meetings are good or bad taking SA of the summaries of the meetings
+    # Last meetings data per each score SA
+    
 class File(models.Model):
   file = models.FileField(blank=False, null=False)
   timestamp = models.DateTimeField(auto_now_add=True)
