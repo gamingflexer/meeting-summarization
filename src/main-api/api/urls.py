3cd7246cf4ff31d36f8aaabbcd491c315eca64d9
Yash-Wakekar <91482798+Yash-Wakekar@users.noreply.github.com>
2023-03-11 21:01:37 +0530

    calender api

--- src/main-api/api/urls.py
+++ src/main-api/api/urls.py
@@ -1,15 +1,24 @@
 from django.urls import path
 
 from .views import LandingPageAPI,AddMeetingAPI,SummaryPageAPI,AddMeetingFileAPI,EditUserDataAPI
-from .calender import GoogleCalendarInitView,GoogleCalendarEventsView,GoogleCalendarRedirectView
+from .calender import GoogleCalendarInitView,GoogleCalendarEventsView,GoogleCalendarRedirectView,GoogleCalendarMultipleEventsView
 
 
 urlpatterns = [
+    # Landing Page API
     path('landing', LandingPageAPI.as_view(), name='landing'),
+
+    # Meetings API
     path('addmeeting', AddMeetingAPI.as_view(), name='addmeeting'),
     path('addmeetingfile/<int:meeting_id>', AddMeetingFileAPI.as_view(), name='addmeetingfile'),
+
+    # Summary API
     path('summary/<int:meeting_id>', SummaryPageAPI.as_view(), name='summary'),
     path('userinfo/<str:username>', EditUserDataAPI.as_view(), name='fetch_user_data'),
+    path('summary/<int:meeting_id>', SummaryPageAPI.as_view(), name='summary'),
+    path('summary/<int:meeting_id>/pdf', SummaryPageAPI.as_view(), name='summary_pdf'),
+
+    # Google Calendar API
     path('rest/v1/calendar/init/',
          GoogleCalendarInitView.as_view(), name='calendar_init'),
     path('rest/v1/calendar/redirect/',
@@ -18,6 +27,11 @@ urlpatterns = [
          GoogleCalendarEventsView.as_view(), name='calendar_redirect'),
     path('summary/<int:meeting_id>', SummaryPageAPI.as_view(), name='summary'),
     path('summary/<int:meeting_id>/pdf', SummaryPageAPI.as_view(), name='summary_pdf'),
+    path('userinfo/<str:username>', EditUserDataAPI.as_view(), name='fetch_user_data'),
+    path('calendar/multipleevents/<api_keyword>',
+         GoogleCalendarMultipleEventsView.as_view(), name='calendar_upcoming_event'),
+
+    # User API
     path('userinfo/<str:username>', EditUserDataAPI.as_view(), name='fetch_user_data')
 
 ] 
