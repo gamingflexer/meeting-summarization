5ae7e94c2536b945ebd833c5dc8c116c46c8db74
gamingflexer <osurve20ece@student.mes.ac.in>
2023-03-14 10:32:06 +0530

    added video support

--- src/main-api/api/preprocessing/utils.py
+++ src/main-api/api/preprocessing/utils.py
@@ -96,6 +96,7 @@ def transcript_to_dataframe(file_path):
     
     return df
 
+
 def transcript_webvtt_to_dataframe(file_path):
     with open(file_path, 'r') as f:
         lines = f.readlines()
@@ -108,3 +109,66 @@ def transcript_webvtt_to_dataframe(file_path):
     df = pd.DataFrame(rows)
     return df
 
+def segment_transcript(df):
+    # Define the keywords for each section
+    with open('./data/start_meet_keywords.txt', 'r') as f:
+        start_keywords = [line.strip() for line in f.readlines()]
+    with open('./data/main_context_keywords.txt', 'r') as f:
+        context_keywords = [line.strip() for line in f.readlines()]
+    with open('./data/end_meet_keywords.txt', 'r') as f:
+        end_keywords = [line.strip() for line in f.readlines()]
+
+    # Initialize the section markers
+    start_marker = None
+    end_marker = None
+
+    # Iterate through the lines and look for the keywords
+    for i, row in df.iterrows():
+        text = row['text']
+        # Check for start keyword
+        if not start_marker and any(keyword in text.lower() for keyword in start_keywords):
+            start_marker = i
+
+        # Check for end keyword
+        if not end_marker and any(keyword in text.lower() for keyword in end_keywords):
+            end_marker = i
+
+    # Create the segments based on the markers
+    start_segment = df.loc[:start_marker-1] if start_marker is not None else None
+    end_segment = df.loc[end_marker:] if end_marker is not None else None
+    
+    main_context = df.loc[start_marker:end_marker-1] if start_marker is not None and end_marker is not None else None
+    
+    # Add new column to each segment
+    if start_segment is not None:
+        start_segment['main_timestamps'] = 'start'
+    if end_segment is not None:
+        end_segment['main_timestamps'] = 'end'
+    if main_context is not None:
+        main_context['main_timestamps'] = 'main_context'
+    
+    # Combine the segments into a single DataFrame
+    segments = pd.concat([df for df in [start_segment, main_context, end_segment] if df is not None])
+    
+    return segments
+
+def duration_from_transcript(df):
+    try:
+        df['time'] = pd.to_timedelta(df['time_stamp'])
+        # Calculate the end time for each speaker
+        df['end_time'] = df['time_stamp']
+        # Shift the end time by one row to get the start time for each speaker
+        df['start_time'] = df['end_time'].shift()
+        # Calculate the duration for each speaker
+        df['duration'] = df['end_time'] - df['start_time']
+    except Exception as e:
+        print("ERROR !! : ",e)
+        if str(df['start_time'][0]) == "nan":
+            start_time = "00.000"
+            return {"start_time":start_time,"end_time":df['end_time'][df.index[-1]]}
+        return {"start_time":df['start_time'][0],"end_time":df['end_time'][df.index[-1]]}
+    
+    if str(df['start_time'][0]) == "nan":
+            start_time = "00.000"
+            return {"start_time":start_time,"end_time":df['end_time'][df.index[-1]]}
+    return {"start_time":df['start_time'][0],"end_time":df['end_time'][df.index[-1]]}
