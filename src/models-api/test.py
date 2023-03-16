7507864b8b882423bd8ec094b480aa63ffc09bb3
gamingflexer <osurve20ece@student.mes.ac.in>
2023-03-16 07:27:15 +0530

    test cases & whsiper updated

--- src/models-api/test.py
+++ src/models-api/test.py
@@ -1,32 +1,10 @@
 from views.models import ModelSelect
 
 data = {
-    "longformer": [
-        {
-            "model_id": "MingZhong/DialogLED-base-16384",
-            "path": "MingZhong/DialogLED-base-16384",
-            "dataset": "DialogSum",
-            "perfomance": 0,
-            "topics": [],
-            "notes": "large model",
-            "type": "Abstractive"
-        }
-    ],
-    "pegasus": [
-        {
-            "model_id": "google/pegasus-xsum",
-            "path": "hobab185/pegasus-pn_summary",
-            "dataset": "",
-            "perfomance": 0,
-            "topics": [],
-            "notes": "",
-            "type": ""
-        }
-    ],
     "bart": [
         {
-            "model_id": "knkarthick/meeting-summary-samsum",
-            "path": "knkarthick/meeting-summary-samsum",
+            "model_id": "knkarthick/MEETING_SUMMARY",
+            "path": "knkarthick/MEETING_SUMMARY",
             "dataset": "samsum",
             "perfomance": 0,
             "topics": [],
@@ -38,8 +16,8 @@ data = {
 
 for model_name in data:
     for model in data[model_name]:
-        newmodel = ModelSelect(modelname = model_name,
-                            model_id_or_path = model['model_id'])
+        newmodel = ModelSelect(modelname = model_name,text = "dd",
+                            model_id_or_path = model['model_id'],max_new_tokens = 200)
         model = newmodel.load_model()
         del model
 
