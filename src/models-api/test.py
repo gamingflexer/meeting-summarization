42b3ffa86d2a0057266eedefea9a5f359e8962e3
gamingflexer <osurve20ece@student.mes.ac.in>
2023-03-30 13:48:09 +0530

    test cases added

--- src/models-api/test.py
+++ src/models-api/test.py
@@ -3,8 +3,8 @@ from views.models import ModelSelect
 data = {
     "bart": [
         {
-            "model_id": "knkarthick/MEETING_SUMMARY",
-            "path": "knkarthick/MEETING_SUMMARY",
+            "model_id": "asach/bart-highlights-redit",
+            "path": "asach/bart-highlights-redit",
             "dataset": "samsum",
             "perfomance": 0,
             "topics": [],
@@ -21,3 +21,17 @@ for model_name in data:
         model = newmodel.load_model()
         del model
 
+import torch
+from transformers import AutoTokenizer, AutoModel, LongT5ForConditionalGeneration, LEDForConditionalGeneration
+
+# load tokenizer
+# tokenizer = AutoTokenizer.from_pretrained("asach/lognt5-xsum-icsi-400-10")
+# model = LongT5ForConditionalGeneration.from_pretrained("asach/lognt5-xsum-icsi-400-10").to("cuda").half()
+# tokenizer = AutoTokenizer.from_pretrained("asach/lognt5-xsum-icsi-5")
+# model = LongT5ForConditionalGeneration.from_pretrained("asach/lognt5-xsum-icsi-5").to("cuda").half()
+# tokenizer = AutoTokenizer.from_pretrained("asach/led-dialogSum-1epoch")
+# model = LEDForConditionalGeneration.from_pretrained("asach/led-dialogSum-1epoch").to("cuda").half()
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
+model = AutoModelForSeq2SeqLM.from_pretrained("microsoft/GODEL-v1_1-large-seq2seq")
