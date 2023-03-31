b3f9a957cc819b743e732d3f204e3c8a35550132
gamingflexer <osurve20ece@student.mes.ac.in>
2023-04-01 01:17:30 +0530

    refactor changes

--- src/main-api/api/serializers.py
+++ src/main-api/api/serializers.py
@@ -19,7 +19,7 @@ class FileSerializer(serializers.ModelSerializer):
 class CalendarEventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Summary
-        fields = ('meeting_id','calender_meeting_id','user_firebase_token','title','creator','organizer','creation_time','start_time','end_time','attendees_count','meet_link','meet_platform','start_time','end_time')
+        fields = ('meeting_id','calender_meeting_id','user_firebase_token','title','creator','organizer','creation_time','start_time','end_time','attendees_count','meet_link','meet_platform')
         
 class MicrosoftEventSerializer(serializers.ModelSerializer):
     class Meta:
