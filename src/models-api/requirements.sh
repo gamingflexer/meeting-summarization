e6743bb0e06fe122e5627ffb3e1b7be2ef4f220f
gamingflexer <osurve20ece@student.mes.ac.in>
2023-04-07 01:04:24 +0530

    req updated

--- src/models-api/requirements.sh
+++ src/models-api/requirements.sh
@@ -1,5 +1,6 @@
 pip install git+https://github.com/openai/whisper.git --no-deps
 python -m spacy download en_core_web_sm
+python -m spacy download en_core_web_lg
 pip install -U scikit-learn scipy matplotlib
 git clone https://github.com/maxent-ai/converse.git && cd converse && pip install -e . && cd ..
 sudo apt install ffmpeg
