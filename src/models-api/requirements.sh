bea055dee96408952b17eb73b635997d9d2e6af5
gamingflexer <osurve20ece@student.mes.ac.in>
2023-04-08 07:34:29 +0530

    req updated

--- src/models-api/requirements.sh
+++ src/models-api/requirements.sh
@@ -4,3 +4,4 @@ python -m spacy download en_core_web_lg
 pip install -U scikit-learn scipy matplotlib
 git clone https://github.com/maxent-ai/converse.git && cd converse && pip install -e . && cd ..
 sudo apt install ffmpeg
+sudo apt-get install libsndfile1-dev
